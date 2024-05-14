a9b92d7f31fa55197e5e1105f18ecc45c932b138
sallen1851 <sallen1851@gmail.com>
2024-05-13 22:03:09 -0400

    create Game class, createPhrases array

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+// Create a Game class methods for starting and ending the game, handling interactions, getting a random phrase, checking for a win, and removing a life from the scoreboard.
+
+class Game { 
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    };
+
+// Creates phrases for use in game
+    createPhrases() {
+        const phrases = [
+            'May the Force be with you',
+            'You talking to me',
+            'Here is looking at you kid',
+            'You cant handle the truth',
+            'I see dead people'
+        ];
+        console.log('Phrases array:', phrases);
+    };
