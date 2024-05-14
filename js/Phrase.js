d698861dc4d421cf18e637e37817a62b0a3ed950
sallen1851 <sallen1851@gmail.com>
2024-05-13 21:44:53 -0400

    added addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,3 +6,34 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }; 
+
+
+    // Adds letter placeholders to the display when the game starts
+    addPhraseToDisplay() {
+        const phrase = this.phrase;
+        console.log('Phrase:', phrase); 
+    
+        const phraseUl = document.querySelector('#phrase ul');
+        console.log('Selected UL element:', phraseUl);
+    
+        for (let i = 0; i < phrase.length; i++) {
+            let letter = phrase[i];
+            const letterLi = document.createElement('li');
+    
+            console.log('Processing letter:', letter); 
+    
+            if (letter === ' ') {
+                letterLi.className = 'space';
+                letterLi.textContent = ' ';
+                console.log('Added space element:', letterLi); 
+            } else {
+                letterLi.className = `hide letter ${letter}`;
+                letterLi.textContent = `${letter}`;
+                console.log('Added letter element:', letterLi); 
+            }
+    
+            phraseUl.appendChild(letterLi);
+        };
+    
+        return phrase;
+    };
