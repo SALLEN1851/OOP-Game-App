a7ffd31b1326ef7db458c4997c2824543eb60f1a
sallen1851 <sallen1851@gmail.com>
2024-05-14 21:23:07 -0400

    function to start game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+let game;
+
+// Function to start a new game
+
+const startGame = () => {
+    console.log("Starting a new game");
+    game = new Game();
+    game.startGame();
+};  
+
+startGame();
