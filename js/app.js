3ceb7ff57bca2af3972557448129758829bd3a0b
sallen1851 <sallen1851@gmail.com>
2024-05-14 21:43:50 -0400

    added event listeners for onscreen keyboard and physical keyboard interation

--- js/app.js
+++ js/app.js
@@ -3,13 +3,48 @@
  * app.js */
 
 let game;
+const startGameButton = document.getElementById('#btn__reset');
+const keyboardDiv = document.getElementById('#qwerty');
+const keysArray = document.querySelectorAll('.key');
+const overlayDiv = document.querySelector('#overlay');
 
 // Function to start a new game
-
 const startGame = () => {
     console.log("Starting a new game");
     game = new Game();
     game.startGame();
 };  
 
-startGame();
+// Function to handle onscreen keyboard button clicks
+const handleInteraction = (target) => {
+    game.handleInteraction(target);
+};
+
+// Adds a event listener to the "Start Game" button when clicked
+startGameButton.addEventListener('click', startGame);
+
+
+// Adds click event listerners to each of the onscreen keyboard buttons
+keyboardDiv.addEventListener('click', (e) => {
+    if(e.target.className === 'key') {
+        handleInteraction(e.target);
+    }
+});
+
+
+// Adds keyup event listener to the physical keyboard
+document.addEventListerner('keyup', (e) => {
+    // allows use of physical keyboard to interact with the game
+    if (overlayDiv.style.display === 'none') {
+        keysArray.forEach((key) => { 
+            if (e.key === key.textContent) {
+                handleInteraction(key);
+            }           
+        });
+    }
+
+    // Allows the Enter key to be used to start a new game
+    if (overlayDiv.style.display !== 'none' && e.key === 'Enter') {
+        startGame();
+    }
+});
